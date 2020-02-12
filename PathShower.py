52f073d89e36170a4c249bb0ed1f34c6b7238243
Stasolet <stasolet@gmail.com>
2020-02-12 20:59:13 +0300

    Реализовано управление составом маршрута

--- PathShower.py
+++ PathShower.py
@@ -1,15 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QComboBox, QLineEdit, QSizePolicy, QLabel
 
 from ViewShower import ViewShower, ViewInfoChanger, ViewRecordAdder
-# todo  Где-то должен отображаться состав маршрута
-#  также должно реализовано возможность изменения и добавления вместе с составом
-#  какая-то жопная это часть, но ладно
-
-# вывести состав маршрута
-# кнопка добавления пункта
-# учёт где начало где конец
-# чуть накрученный push
-
 path_combo_config = {"Станция отправления": ("станция_view", "*", "`Населённый пункт`", "Станция отправления"),
                      "Станция прибытия": ("станция_view", "*", "`Населённый пункт`", "Станция прибытия")}
 
@@ -20,44 +11,65 @@ class PathEditor(ViewInfoChanger):
     def __init__(self, header, info, parent: ViewShower):
         super().__init__(header, info, parent)
         self.way_layout = QVBoxLayout()
-        self.path_idx = info[0]
+        push_btn = self.main_layout.takeAt(self.main_layout.count() - 1).widget()
+
+        self.path_id = info[0]
         self.way_change_idx = {}
         self.point_positions = []
 
-        way = self.db.execute(f"SELECT Станция, `Код станции` FROM `состав_маршрута_view` where `Номер маршрута` = {self.path_idx}")
-        for pos, point in enumerate(way):
-            # можно обернуть в функцию
+        add_btn = QPushButton("Добавить")
+        add_btn.clicked.connect(lambda e: self.add_cell(-1))
+        self.main_layout.addWidget(add_btn)
+
+        way = self.db.execute(f"SELECT Станция, `Код станции` FROM `состав_маршрута_view` where `Номер маршрута` = {self.path_id}")
+        for pos, point in enumerate(way, start=-1):
             station_info = str(point[0])
-            self.point_positions.append(pos)
             self.combo_change_idx["Станция отправления"][station_info] = point[1]
-            cell = QHBoxLayout()
-            edi = QLineEdit()
-            edi.setText(station_info)
+            self.add_cell(pos, station_info)
 
-            add_btn = QPushButton("Добавить")
-            down_btn = QPushButton("Вниз")
-            up_btn = QPushButton("Вверх")
-
-            cmb = QComboBox()
-            cmb.addItem(station_info)
-
-            up_btn.clicked.connect(lambda e, p=pos, c=cell: self.move_cell_up(c))
-            down_btn.clicked.connect(lambda e, p=pos, c=cell: self.move_cell_down(c))
-            add_btn.clicked.connect(lambda e, c=cell: self.add_cell(cell.pos))
-
-            edi.editingFinished.connect(lambda c=cmb, t=edi.text:
-                                        self.combo_update("Станция отправления", c, t()))  # le-kostyl
-            cell.pos = pos
-            cell.addWidget(edi)
-            cell.addWidget(cmb)
-            cell.addWidget(add_btn)
-            cell.addWidget(down_btn)
-            cell.addWidget(up_btn)
-            self.way_layout.addLayout(cell)
         self.main_layout.addLayout(self.way_layout)
+        self.main_layout.addWidget(push_btn)
 
-    def add_cell(self, idx: int):
-        pass
+    def add_cell(self, pos: int, txt=""):
+        """Вставляет ячейку ниже активирующей кнопки для вставки на уровне надо передать ::pos:: = -1"""
+
+        cell = QHBoxLayout()
+        edi = QLineEdit()
+        edi.setText(txt)
+
+        add_btn = QPushButton("Добавить")
+        down_btn = QPushButton("Вниз")
+        up_btn = QPushButton("Вверх")
+
+        cmb = QComboBox()
+        cmb.addItem(txt)
+
+        edi.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
+        cmb.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)  # это почти работает todo исправить размер
+
+        up_btn.clicked.connect(lambda e, p=pos, c=cell: self.move_cell_up(c))
+        down_btn.clicked.connect(lambda e, p=pos, c=cell: self.move_cell_down(c))
+
+        add_btn.clicked.connect(lambda e, c=cell: self.add_cell(c.pos))
+        # todo кнопка удаления путевой точки
+        edi.editingFinished.connect(lambda c=cmb, t=edi.text:
+                                    self.combo_update("Станция отправления", c, t()))  # le-kostyl
+        cell.pos = pos
+        cell.addWidget(edi)
+        cell.addWidget(cmb)
+        cell.addWidget(add_btn)
+        cell.addWidget(down_btn)
+        cell.addWidget(up_btn)
+        for i in range(pos + 1, self.way_layout.count()):
+            cell_to_move = self.way_layout.itemAt(i)
+            cell_to_move.pos += 1
+        cell.pos += 1  # для вставки ниже активированной кнопки
+
+        self.way_layout.insertLayout(cell.pos, cell)
+        for i in range(self.way_layout.count()):
+            ccc = self.way_layout.itemAt(i)
+            print(ccc.pos)
+        print("****")
 
     def move_cell_up(self, cell):
         if cell.pos > 0:
@@ -75,19 +87,26 @@ class PathEditor(ViewInfoChanger):
             old_cell.pos -= 1
             cell.pos += 1
 
-    def push_all(self):
+    def push_point_changes(self):
         params = []
+        station_ord_numb_in_path = 0
         for i in range(self.way_layout.count()):
             cell = self.way_layout.itemAt(i)
             w = cell.itemAt(1).widget()
             station = w.currentText()
             if station:
-                idx = self.combo_change_idx["Станция отправления"][station]
-                params.append((self.path_idx, i, idx))
+                station_id = self.combo_change_idx["Станция отправления"][station]
+                params.append((self.path_id, station_ord_numb_in_path, station_id))
+                station_ord_numb_in_path += 1
 
         query = f" insert into `состав маршрута` values(%s, %s, %s)"
-        self.db.execute("delete from `состав маршрута` where `Номер маршрута` = %s", (self.path_idx,))
+        self.db.execute("delete from `состав маршрута` where `Номер маршрута` = %s", (self.path_id,))
         self.db.executemany(query, params)
+        self.db.commit()
+
+    def push_changes(self):
+        self.push_point_changes()
+        super().push_changes()
 
 
 class PathShower(ViewShower):
