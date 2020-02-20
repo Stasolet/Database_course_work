804d114d655a39c8cef657416fb5c530c6c4e673
Stasolet <stasolet@gmail.com>
2020-02-20 22:43:30 +0300

    Добавлены виджеты статистики

--- MainUi.py
+++ MainUi.py
@@ -4,6 +4,7 @@ from AutoUi import AutoUi
 from DriverUi import DriverUi
 from OrdersUi import OrdersUi
 from PathUi import PathUi
+from StatisticUi import StatisticUi
 
 
 class MainUi(QWidget):
@@ -13,23 +14,25 @@ class MainUi(QWidget):
 
         self.resize(500, 500)
 
-        # TODO часть виджетов -- просто одна кнопка, создающая TableShower, исправить
         self.drive_btn = QPushButton("Управление персоналом")
         self.auto_btn = QPushButton("Управление автопарком")
         self.orders_btn = QPushButton("Управление рейсами")
         self.path_btn = QPushButton("Управлнение маршрутами")
+        self.statistic_btn = QPushButton("Статистика")
 
         self.drive_btn.clicked.connect(lambda: self.slave_widgets.append(DriverUi()))
         self.auto_btn.clicked.connect(lambda: self.slave_widgets.append(AutoUi()))
         self.orders_btn.clicked.connect(lambda: self.slave_widgets.append(OrdersUi()))
         self.path_btn.clicked.connect(lambda: self.slave_widgets.append(PathUi()))
+        self.statistic_btn.clicked.connect(lambda: self.slave_widgets.append(StatisticUi()))
 
         self.bx = QVBoxLayout()
         self.bx.addWidget(self.drive_btn)
         self.bx.addWidget(self.auto_btn)
         self.bx.addWidget(self.orders_btn)
         self.bx.addWidget(self.path_btn)
+        self.bx.addWidget(self.statistic_btn)
 
         self.setLayout(self.bx)
-
         self.show()
+
---
+++ StatisticUi.py
@@ -0,0 +1,53 @@
+from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout
+
+from TableShower import TableShower
+from ProcedureShower import ProcedureShower
+
+driver_statistic_args = [("Водитель",
+                          {"src": "водитель",
+                              "comb_info": "CONCAT(`Фамилия`, ' ',`Имя`, ' ',`Отчество`)",
+                               "substitution": "`Табельный номер`"})]
+
+auto_statistic_args = [("Автомобиль",
+                        {"src": "автомобиль",
+                                "comb_info": """CONCAT(`Номер автомобиля`, ' ', `Производитель`, ' ', `Модель`,
+                                                ' ', `Вместимость`, ' мест')""",
+                                "substitution": "`Номер автомобиля`"})]
+
+
+class StatisticUi(QWidget):
+    def add_slave(self, widget: QWidget):
+        self.slave_widgets.append(widget)
+        widget.show()
+
+    def __init__(self):
+        super().__init__()
+        self.slave_widgets = []
+        self.full_stat = QPushButton("Общая статистика")
+        self.full_auto_stat = QPushButton("Статистика по автомобилям")
+        self.full_drivers_stat = QPushButton("Статистика по водителям")
+        self.one_auto_stat = QPushButton("Статистика по одному автомобилям")
+        self.one_drive_stat = QPushButton("Статистика по одному водителю")
+
+        self.full_stat.clicked.connect(lambda: self.add_slave(TableShower("`общая статистика по автомобилям`",
+                                                                          editable=False)))
+        self.full_auto_stat.clicked.connect(lambda: self.add_slave(TableShower("`общая статистика по водителям`",
+                                                                               editable=False)))
+        self.full_drivers_stat.clicked.connect(lambda: self.add_slave(TableShower("`общая статистика помесячно`",
+                                                                                  editable=False)))
+        self.one_auto_stat.clicked.connect(lambda: self.add_slave(ProcedureShower("auto_statistic",
+                                                                                  arg=auto_statistic_args)))
+        self.one_drive_stat.clicked.connect(lambda: self.add_slave(ProcedureShower("driver_statistic",
+                                                                                   arg=driver_statistic_args)))
+
+        self.box = QVBoxLayout()
+        self.box.addWidget(self.full_stat)
+        self.box.addWidget(self.full_auto_stat)
+        self.box.addWidget(self.full_drivers_stat)
+        self.box.addWidget(self.one_auto_stat)
+        self.box.addWidget(self.one_drive_stat)
+
+        self.setLayout(self.box)
+
+        self.show()
+
