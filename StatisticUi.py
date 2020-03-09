4cc98ee3505eddff58f026deb66a4fc85d426ef2
Stasolet <stasolet@gmail.com>
2020-03-09 18:56:49 +0300

    Добавлена статистика по маршрутам

--- StatisticUi.py
+++ StatisticUi.py
@@ -14,6 +14,11 @@ auto_statistic_args = [("Автомобиль",
                                                 ' ', `Вместимость`, ' мест')""",
                                 "substitution": "`Номер автомобиля`"})]
 
+path_statistic_args = [("Маршрут",
+                        {"src": "маршрут_view",
+                         "comb_info": "CONCAT(`Станция отправления`, ' - ', `Станция прибытия`)",
+                         "substitution": "`Номер маршрута`"})]
+
 
 class StatisticUi(QWidget):
     def add_slave(self, widget: QWidget):
@@ -26,8 +31,11 @@ class StatisticUi(QWidget):
         self.full_stat = QPushButton("Общая статистика")
         self.full_auto_stat = QPushButton("Статистика по автомобилям")
         self.full_drivers_stat = QPushButton("Статистика по водителям")
+        self.full_path_stat = QPushButton("Статистика по маршрутам")
+
         self.one_auto_stat = QPushButton("Статистика по одному автомобилям")
         self.one_drive_stat = QPushButton("Статистика по одному водителю")
+        self.one_path_stat = QPushButton("Статистика по одному маршруту")
 
         self.full_stat.clicked.connect(lambda: self.add_slave(TableShower("`общая статистика помесячно`",
                                                                           editable=False, deletable=False)))
@@ -35,17 +43,24 @@ class StatisticUi(QWidget):
                                                                                editable=False, deletable=False)))
         self.full_drivers_stat.clicked.connect(lambda: self.add_slave(TableShower("`общая статистика по водителям`",
                                                                                   editable=False, deletable=False)))
+        self.full_path_stat.clicked.connect(lambda: self.add_slave(TableShower("`общая статистика по маршрутам`",
+                                                                               editable=False, deletable=False)))
+
         self.one_auto_stat.clicked.connect(lambda: self.add_slave(ProcedureShower("auto_statistic",
                                                                                   arg=auto_statistic_args)))
         self.one_drive_stat.clicked.connect(lambda: self.add_slave(ProcedureShower("driver_statistic",
                                                                                    arg=driver_statistic_args)))
+        self.one_path_stat.clicked.connect(lambda: self.add_slave(ProcedureShower("path_statistic",
+                                                                                  arg=path_statistic_args)))
 
         self.box = QVBoxLayout()
         self.box.addWidget(self.full_stat)
         self.box.addWidget(self.full_auto_stat)
         self.box.addWidget(self.full_drivers_stat)
+        self.box.addWidget(self.full_path_stat)
         self.box.addWidget(self.one_auto_stat)
         self.box.addWidget(self.one_drive_stat)
+        self.box.addWidget(self.one_path_stat)
 
         self.setLayout(self.box)
 
